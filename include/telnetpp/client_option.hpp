169c40f3b765f019737800bf066b2d6af9b2c2a7
kazdragon <matthew.chaplain@gmail.com>
2015-10-06 13:23:48 +0000

    Also added client option.

---
+++ include/telnetpp/client_option.hpp
@@ -0,0 +1,73 @@
+#ifndef TELNETPP_client_OPTION
+#define TELNETPP_client_OPTION
+
+#include "telnetpp/core.hpp"
+#include "telnetpp/token.hpp"
+#include <boost/signals2.hpp>
+#include <vector>
+
+namespace telnetpp {
+
+class client_option {
+public :
+    //* =====================================================================
+    /// \brief Constructor
+    //* =====================================================================
+    client_option(telnetpp::u8 option);
+    
+    //* =====================================================================
+    /// \brief Returns the option code.
+    //* =====================================================================
+    telnetpp::u8 option() const;
+    
+    //* =====================================================================
+    /// \brief Activates the option.
+    //* =====================================================================
+    std::vector<telnetpp::token> activate();
+    
+    //* =====================================================================
+    /// \brief Deactivates the option.
+    //* =====================================================================
+    std::vector<telnetpp::token> deactivate();
+
+    //* =====================================================================
+    /// \brief Returns true iff the option is active.
+    //* =====================================================================
+    bool is_active() const;
+
+    //* =====================================================================
+    /// \brief Makes a request of the option
+    //* =====================================================================
+    std::vector<telnetpp::token> negotiate(telnetpp::u8 request);
+    
+    //* =====================================================================
+    /// \brief Send a subnegotiation to the option.
+    //* =====================================================================
+    void subnegotiate(std::vector<telnetpp::u8> const &content);
+
+    boost::signals2::signal<void (telnetpp::u16, telnetpp::u16)> on_window_size_changed;
+    boost::signals2::signal<void ()> on_state_changed;
+    
+private :
+    //* =====================================================================
+    /// \brief Handle a negotiation that has been received in the active
+    /// state.
+    //* =====================================================================
+    virtual void handle_subnegotiation(
+        std::vector<telnetpp::u8> const &content) = 0;
+
+    enum class state
+    {
+        inactive,
+        activating,
+        active,
+        deactivating,
+    };
+    
+    state state_ = state::inactive;
+    u8 option_;
+};
+
+}
+
+#endif
---
+++ src/client_option.cpp
@@ -0,0 +1,141 @@
+#include "telnetpp/client_option.hpp"
+#include "telnetpp/protocol.hpp"
+
+namespace telnetpp {
+
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
+client_option::client_option(u8 option)
+  : option_(option)
+{
+}
+
+// ==========================================================================
+// OPTION
+// ==========================================================================
+u8 client_option::option() const
+{
+    return option_;
+}
+
+// ==========================================================================
+// ACTIVATE
+// ==========================================================================
+std::vector<telnetpp::token> client_option::activate()
+{
+    if (state_ == state::inactive)
+    {
+        state_ = state::activating;
+        return { telnetpp::negotiation(telnetpp::do_, option_) };
+    }
+    else
+    {
+        if (state_ == state::active)
+        {
+            on_state_changed();
+        }
+        
+        return {};
+    }
+}
+
+// ==========================================================================
+// DEACTIVATE
+// ==========================================================================
+std::vector<telnetpp::token> client_option::deactivate()
+{
+    if (state_ == state::inactive)
+    {
+        on_state_changed();
+        return {};
+    }
+    
+    if (state_ == state::active)
+    {
+        state_ = state::deactivating;
+        return { telnetpp::negotiation(telnetpp::dont, option_) };
+    }
+    else
+    {
+        return {};
+    }
+}
+
+// ==========================================================================
+// IS_ACTIVE
+// ==========================================================================
+bool client_option::is_active() const
+{
+    return state_ == state::active;
+}
+
+// ==========================================================================
+// NEGOTIATE
+// ==========================================================================
+std::vector<telnetpp::token> client_option::negotiate(telnetpp::u8 request)
+{
+    switch(state_)
+    {
+        case state::inactive :
+            return { 
+                telnetpp::negotiation(telnetpp::dont, option_)
+            };
+            
+        case state::activating :
+            if (request == telnetpp::will)
+            {
+                state_ = state::active;
+                on_state_changed();
+            }
+            else
+            {
+                state_ = state::inactive;
+                on_state_changed();
+            }
+            
+            return {};
+
+        case state::active :
+            if (request == telnetpp::will)
+            {
+                return { telnetpp::negotiation(telnetpp::do_, option_) };
+            }
+            else
+            {
+                state_ = state::inactive;
+                return { telnetpp::negotiation(telnetpp::dont, option_) };
+            }
+            
+        case state::deactivating :
+            if (request == telnetpp::will)
+            {
+                // NOTE: This is technically unspecified.  No client would send
+                // a WILL when we're already active, and they're not allowed to
+                // send a WILL after receiving a DONT.  But to be nice, we'll
+                // re-activate.
+                state_ = state::active;
+                on_state_changed();
+            }
+            else if (request == telnetpp::wont)
+            {
+                state_ = state::inactive;
+                on_state_changed();
+            }
+                
+            return {};
+    }
+}
+
+// ==========================================================================
+// SUBNEGOTIATE
+// ==========================================================================
+void client_option::subnegotiate(std::vector<telnetpp::u8> const &content)
+{
+    if (state_ == state::active)
+    {
+        handle_subnegotiation(content);
+    }
+}
+
+}
