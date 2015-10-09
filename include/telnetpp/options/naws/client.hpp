c6d87083713566db820ce3cde4bbb6c6c1a5f4a6
kazdragon <matthew.chaplain@gmail.com>
2015-10-09 06:29:35 +0000

    Added NAWS client.

---
+++ include/telnetpp/options/naws/client.hpp
@@ -0,0 +1,40 @@
+#ifndef TELNETPP_OPTIONS_NAWS_CLIENT
+#define TELNETPP_OPTIONS_NAWS_CLIENT
+
+#include "telnetpp/client_option.hpp"
+#include <boost/optional.hpp>
+#include <utility>
+
+namespace telnetpp { namespace options { namespace naws {
+
+class client : public telnetpp::client_option {
+public :
+    //* =====================================================================
+    /// CONSTRUCTOR
+    //* =====================================================================
+    client();
+
+    //* =====================================================================
+    /// \brief Sets the window size that this option will report.
+    //* =====================================================================
+    std::vector<telnetpp::token> set_window_size(u16 width, u16 height);
+
+private :
+    //* =====================================================================
+    /// \brief Handle a negotiation that has been received in the active
+    /// state.
+    //* =====================================================================
+    std::vector<telnetpp::token> handle_subnegotiation(
+        std::vector<telnetpp::u8> const &content) override;
+
+    //* =====================================================================
+    /// \brief Report the window size.
+    //* =====================================================================
+    std::vector<telnetpp::token> report_window_size();
+    
+    boost::optional<std::pair<telnetpp::u16, telnetpp::u16>> window_size_;
+};
+
+}}}
+
+#endif
---
+++ src/options/naws/client.cpp
@@ -0,0 +1,62 @@
+#include "telnetpp/options/naws/client.hpp"
+#include "telnetpp/options/naws.hpp"
+
+namespace telnetpp { namespace options { namespace naws {
+
+// ==========================================================================
+// CONSTRUCTOR
+// ==========================================================================
+client::client()
+  : client_option(telnetpp::options::naws::option)
+{
+    on_state_changed.connect(
+        [this]()
+        {
+            return report_window_size();
+        });
+}
+
+// ==========================================================================
+// SET_WINDOW_SIZE
+// ==========================================================================
+std::vector<telnetpp::token> client::set_window_size(u16 width, u16 height)
+{
+    window_size_ = std::make_pair(width, height);
+    return report_window_size();
+}
+
+// ==========================================================================
+// HANDLE_SUBNEGOTIATION
+// ==========================================================================
+std::vector<telnetpp::token> client::handle_subnegotiation(
+    std::vector<telnetpp::u8> const &content)
+{
+    return {};
+}
+
+// ==========================================================================
+// REPORT_WINDOW_SIZE
+// ==========================================================================
+std::vector<telnetpp::token> client::report_window_size()
+{
+    if (is_active() && window_size_.is_initialized())
+    {
+        return 
+        {
+            telnetpp::subnegotiation(
+                option(),
+                {
+                    telnetpp::u8(window_size_->first >> 8),
+                    telnetpp::u8(window_size_->first & 0xFF),
+                    telnetpp::u8(window_size_->second >> 8),
+                    telnetpp::u8(window_size_->second & 0xFF)
+                })
+        };
+    }
+    else
+    {
+        return {};
+    }
+}
+
+}}}
