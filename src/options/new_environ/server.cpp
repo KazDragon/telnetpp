8a6930cd264ab322cd529403a588f8855046bd90
Matthew Chaplain <matthew.chaplain@gmail.com>
2015-12-22 19:48:08 +0100

    Fleshed out sending mechanism.

--- src/options/new_environ/server.cpp
+++ src/options/new_environ/server.cpp
@@ -17,11 +17,33 @@ server::server()
 // ==========================================================================
 std::vector<token> server::request_variables(std::vector<request> const &requests)
 {
+    telnetpp::u8stream content;
+    content.push_back(telnetpp::options::new_environ::send);
+    
+    for(auto &req : requests)
+    {
+        content.push_back(req.type);
+        
+        for (auto &ch : req.name)
+        {
+            switch (ch)
+            {
+                case telnetpp::options::new_environ::var : // Fall-through
+                case telnetpp::options::new_environ::value : // Fall-through
+                case telnetpp::options::new_environ::esc : // Fall-through
+                case telnetpp::options::new_environ::uservar :
+                    // Fall-through
+                    content.push_back(telnetpp::options::new_environ::esc);
+                default :
+                    content.push_back(ch);
+                    break;
+            }
+        }                
+    }
+
     return {
         telnetpp::element { 
-            telnetpp::subnegotiation(
-                option(), 
-                { telnetpp::options::new_environ::send })
+            telnetpp::subnegotiation(option(), content)
         }
     };
 }
--- test/new_environ_server_test.cpp
+++ test/new_environ_server_test.cpp
@@ -20,3 +20,165 @@ TEST(new_environ_server_test, requesting_no_variables_requests_empty_list)
         },
         server.request_variables({}));
 }
+
+TEST(new_environ_server_test, requesting_one_var_variable_requests_user_variable)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x00, 'T', 'E', 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::var,
+                "TEST"
+            }}));
+}
+
+TEST(new_environ_server_test, requesting_one_user_variable_requests_user_variable)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x03, 'T', 'E', 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::uservar,
+                "TEST"
+            }}));
+}
+
+TEST(new_environ_server_test, requesting_two_variables_requests_two_variables)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x00, 'T', 'E', 'S', 'T', '0',
+                    0x03, 'T', 'E', 'S', 'T', '1'
+                })
+        },
+        server.request_variables(
+            {
+                {
+                    telnetpp::options::new_environ::var,
+                    "TEST0"
+                },
+                {
+                    telnetpp::options::new_environ::uservar,
+                    "TEST1"
+                },
+                
+            }));
+}
+
+TEST(new_environ_server_test, requesting_uservar_with_var_in_name_is_escaped)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x03, 'T', 'E', 0x02, 0x00, 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::uservar,
+                std::string("TE\x00ST", 5)
+            }}));
+}
+
+TEST(new_environ_server_test, requesting_uservar_with_value_in_name_is_escaped)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x03, 'T', 'E', 0x02, 0x01, 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::uservar,
+                std::string("TE\x01ST", 5)
+            }}));
+}
+
+TEST(new_environ_server_test, requesting_uservar_with_esc_in_name_is_escaped)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x03, 'T', 'E', 0x02, 0x02, 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::uservar,
+                std::string("TE\x02ST", 5)
+            }}));
+}
+
+TEST(new_environ_server_test, requesting_uservar_with_uservar_in_name_is_escaped)
+{
+    telnetpp::options::new_environ::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    expect_elements(
+        {
+            telnetpp::subnegotiation(
+                telnetpp::options::new_environ::option,
+                { 
+                    0x01,
+                    0x03, 'T', 'E', 0x02, 0x03, 'S', 'T'
+                })
+        },
+        server.request_variables(
+            {{
+                telnetpp::options::new_environ::uservar,
+                std::string("TE\x03ST", 5)
+            }}));
+}
