22227d6a208fa7118d80b04f1c6479acc9aea299
kazdragon <matthew.chaplain@gmail.com>
2016-03-18 16:07:35 +0000

    Added test for sending multiple MSDP variables.

--- test/msdp_server_test.cpp
+++ test/msdp_server_test.cpp
@@ -88,3 +88,48 @@ TEST(msdp_server_test, send_with_table_sends_table_variable)
         },
         server.send({ variable }));
 }
+
+TEST(msdp_server_test, send_with_many_items_sends_many_items)
+{
+    telnetpp::options::msdp::server server;
+    server.activate();
+    server.negotiate(telnetpp::do_);
+    
+    auto variable0 = telnetpp::options::msdp::variable{"svar", "sval"};
+
+    auto variable1 = telnetpp::options::msdp::variable{
+        "avar", telnetpp::options::msdp::make_value({ "aval0", "aval1" })
+    };
+
+    auto variable2 = telnetpp::options::msdp::variable{
+        "var",
+        telnetpp::options::msdp::make_value({{
+            "tbl", 
+            telnetpp::options::msdp::make_value({ "val0", "val1" })
+        }})
+    };
+ 
+    expect_elements(
+        {
+            telnetpp::subnegotiation{
+                telnetpp::options::msdp::option,
+                { 1, 's', 'v', 'a', 'r',
+                  2, 's', 'v', 'a', 'l',
+                  1, 'a', 'v', 'a', 'r',
+                  2, 5, 2, 'a', 'v', 'a', 'l', '0',
+                        2, 'a', 'v', 'a', 'l', '1',
+                     6,
+                  1, 'v', 'a', 'r', 
+                  2,  3,
+                          1, 't', 'b', 'l',
+                          2, 5,
+                                  2, 'v', 'a', 'l', '0', 
+                                  2, 'v', 'a', 'l', '1',
+                             6,
+                      4
+                }
+            }
+        },
+        server.send({ variable0, variable1, variable2 }));
+   
+}
