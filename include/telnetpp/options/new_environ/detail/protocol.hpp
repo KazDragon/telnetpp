c5a83f7c7603c23792d34bcf4bfa58b5d37794c3
Matthew Chaplain <matthew.chaplain@gmail.com>
2016-05-17 21:40:04 +0200

    Documented the telnetpp::options::new_environ namespace.

--- include/telnetpp/options/new_environ/detail/protocol.hpp
+++ include/telnetpp/options/new_environ/detail/protocol.hpp
@@ -3,6 +3,32 @@
 #include <boost/optional.hpp>
 #include <string>
 
+//* =========================================================================
+/// \namespace telnetpp::options::new_environ
+/// \brief An implementation of the Telnet Environment option
+/// \par Overview
+/// NEW-ENVIRON is used to send the values of variables from server to 
+/// client.
+/// \par
+/// Variables are transmitted in pairs of 
+/// telnetpp::options::new_environ::request and
+/// telnetpp::options::new_environ::response, where the request is the type
+/// of the variable (either a well known "var" type, the names of which are
+/// limited -- see the specification), or a "uservar" type, which could have
+/// any name.  The response may also contain a string if the variable
+/// existed.
+/// \par Usage
+/// Create a server or client as appropriate.  Install it in a session,
+/// activate as normal.
+/// \par
+/// A client may make a request of the server by calling request_variables().
+/// It may also register for notifications about responses by subscribing to
+/// the on_variable_changed signal.
+/// \par
+/// A server may set or delete variables and user variables, and will 
+/// automatically handle requests sent from a client by using this data.
+/// \see https://tools.ietf.org/html/rfc1572
+//* =========================================================================
 namespace telnetpp { namespace options { namespace new_environ { namespace detail {
 
 static constexpr telnetpp::u8 option  = 39;
