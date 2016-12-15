8db71cffba107a4de83c58c27ab7574af76f9286
kazdragon <matthew.chaplain@gmail.com>
2016-12-15 10:10:04 +0000

    make_lambda_visitor assumes void return type.  Replaced visitor in parse_helper.

--- include/telnetpp/detail/lambda_visitor.hpp
+++ include/telnetpp/detail/lambda_visitor.hpp
@@ -6,36 +6,36 @@ template <typename ReturnType, typename... Lambdas>
 struct lambda_visitor;
 
 template <typename ReturnType, typename Lambda1, typename... Lambdas>
-struct lambda_visitor< ReturnType, Lambda1 , Lambdas...> 
+struct lambda_visitor< ReturnType, Lambda1 , Lambdas...>
   : public lambda_visitor<ReturnType, Lambdas...>, public Lambda1 {
 
     using Lambda1::operator();
     using lambda_visitor< ReturnType , Lambdas...>::operator();
-    lambda_visitor(Lambda1 l1, Lambdas... lambdas) 
+    lambda_visitor(Lambda1 l1, Lambdas... lambdas)
       : Lambda1(l1), lambda_visitor< ReturnType , Lambdas...> (lambdas...)
     {}
 };
 
 
 template <typename ReturnType, typename Lambda1>
-struct lambda_visitor<ReturnType, Lambda1> 
+struct lambda_visitor<ReturnType, Lambda1>
   : public boost::static_visitor<ReturnType>, public Lambda1 {
 
     using Lambda1::operator();
-    lambda_visitor(Lambda1 l1) 
+    lambda_visitor(Lambda1 l1)
       : boost::static_visitor<ReturnType>(), Lambda1(l1)
     {}
 };
 
 
 template <typename ReturnType>
-struct lambda_visitor<ReturnType> 
+struct lambda_visitor<ReturnType>
   : public boost::static_visitor<ReturnType> {
 
     lambda_visitor() : boost::static_visitor<ReturnType>() {}
 };
 
-template <typename ReturnType, typename... Lambdas>
+template <typename ReturnType = void, typename... Lambdas>
 lambda_visitor<ReturnType, Lambdas...> make_lambda_visitor(Lambdas... lambdas) {
     return { lambdas... };
 }
