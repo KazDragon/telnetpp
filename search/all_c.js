985a016388e848724a08c6b0b9948c3dc99f471f
KazDragon <KazDragon@users.noreply.github.com>
2026-04-16 04:37:13 +0000

    deploy: 038246d99629df965df56745ad7dfca97a833a0c

--- search/all_c.js
+++ search/all_c.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['request_0',['request',['../structtelnetpp_1_1options_1_1new__environ_1_1request.html',1,'telnetpp::options::new_environ::request'],['../classtelnetpp_1_1negotiation.html#a67187e94d4e20e6d53dfc07eade3f5d0',1,'telnetpp::negotiation::request()']]],
-  ['request_5fterminal_5ftype_1',['request_terminal_type',['../classtelnetpp_1_1options_1_1terminal__type_1_1client.html#a90342b4f8b2a39b7efed736ea2306dab',1,'telnetpp::options::terminal_type::client']]],
-  ['request_5fvariables_2',['request_variables',['../classtelnetpp_1_1options_1_1new__environ_1_1client.html#a575f383f5044fdeb3ee3fca98c0bd00a',1,'telnetpp::options::new_environ::client']]],
-  ['requirements_3',['Requirements',['../index.html#autotoc_md9',1,'']]],
-  ['resolution_20with_20vcpkg_4',['Dependency Resolution With vcpkg',['../index.html#autotoc_md11',1,'']]],
-  ['response_5',['response',['../structtelnetpp_1_1options_1_1new__environ_1_1response.html',1,'telnetpp::options::new_environ']]],
-  ['roadmap_20progress_6',['Features / Roadmap / Progress',['../index.html#autotoc_md13',1,'']]]
+  ['request_5fcharsets_1',['request_charsets',['../classtelnetpp_1_1options_1_1charset_1_1server.html#aec87778e526a2126331b590e90e1056c',1,'telnetpp::options::charset::server']]],
+  ['request_5fterminal_5ftype_2',['request_terminal_type',['../classtelnetpp_1_1options_1_1terminal__type_1_1client.html#a90342b4f8b2a39b7efed736ea2306dab',1,'telnetpp::options::terminal_type::client']]],
+  ['request_5fvariables_3',['request_variables',['../classtelnetpp_1_1options_1_1new__environ_1_1client.html#a575f383f5044fdeb3ee3fca98c0bd00a',1,'telnetpp::options::new_environ::client']]],
+  ['requirements_4',['Requirements',['../index.html#autotoc_md9',1,'']]],
+  ['resolution_20with_20vcpkg_5',['Dependency Resolution With vcpkg',['../index.html#autotoc_md11',1,'']]],
+  ['response_6',['response',['../structtelnetpp_1_1options_1_1new__environ_1_1response.html',1,'telnetpp::options::new_environ']]],
+  ['roadmap_20progress_7',['Features / Roadmap / Progress',['../index.html#autotoc_md13',1,'']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['request_0',['request',['../classtelnetpp_1_1negotiation.html#a67187e94d4e20e6d53dfc07eade3f5d0',1,'telnetpp::negotiation']]],
-  ['request_5fterminal_5ftype_1',['request_terminal_type',['../classtelnetpp_1_1options_1_1terminal__type_1_1client.html#a90342b4f8b2a39b7efed736ea2306dab',1,'telnetpp::options::terminal_type::client']]],
-  ['request_5fvariables_2',['request_variables',['../classtelnetpp_1_1options_1_1new__environ_1_1client.html#a575f383f5044fdeb3ee3fca98c0bd00a',1,'telnetpp::options::new_environ::client']]]
+  ['request_5fcharsets_1',['request_charsets',['../classtelnetpp_1_1options_1_1charset_1_1server.html#aec87778e526a2126331b590e90e1056c',1,'telnetpp::options::charset::server']]],
+  ['request_5fterminal_5ftype_2',['request_terminal_type',['../classtelnetpp_1_1options_1_1terminal__type_1_1client.html#a90342b4f8b2a39b7efed736ea2306dab',1,'telnetpp::options::terminal_type::client']]],
+  ['request_5fvariables_3',['request_variables',['../classtelnetpp_1_1options_1_1new__environ_1_1client.html#a575f383f5044fdeb3ee3fca98c0bd00a',1,'telnetpp::options::new_environ::client']]]
 ];
